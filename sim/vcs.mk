f2939e322e06cc1d62922c47266c42486e515ad8
raymondngarcia <sup3r_princ3@yahoo.com>
2024-04-30 20:50:17 +0200

    delete unnecessary files

--- sim/vcs.mk
+++ sim/vcs.mk
@@ -2,9 +2,34 @@
 ##@ VCS setup:
 ########################################
 
-##% VCS_FLOW_CONFIG @@ ="vcs_config.mk" @@ Specify configuration options
-VCS_FLOW_CONFIG ?= vcs_config.mk
--include $(VCS_FLOW_CONFIG)
+# UVM configuration
+VCS_VLOGAN_OPTS += +incdir+${VCS_HOME}/etc/uvm-1.2
+VCS_ELAB_OPTS   += -ntb_opts uvm-1.2
+VCS_VLOGAN_OPTS += -ntb_opts uvm-1.2
+
+# Specify the debug access:
+VCS_VLOGAN_OPTS += +define+SVT_UVM_TECHNOLOGY
+
+ifeq (${RUN_UNR}, 0)
+  TOP_LEVEL_MODULES  = hdl_top
+  VCS_ELAB_OPTS     += -top hdl_top
+endif #RUN_UNR
+
+NO_DUT           ?= 0
+
+ifeq ($(REGRESSION), 1)
+  VCS_RUN_OPTS += +REGRESSION=1
+endif
+
+# SIM RUN
+VCS_RUN_OPTS         += -reportstats
+
+ifeq ("$(VCS_DUMP_TYPE)", "vpd")
+ifeq ("$(VCS_GUI_TYPE)", "dve")
+VCS_ELAB_OPTS += -debug_access+all
+VCS_RUN_OPTS_TCL = -ucli -do ../../dump_vpd.tcl
+endif
+endif
 
 ########################################
 ##@ VCS build variables:
